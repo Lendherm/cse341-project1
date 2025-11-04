a190e70678e75eff91407ef5facfb6e2f3764feb
Luis Emilio Nathanael De la rosa Meneses <dluisemilionathanael@gmail.com>
2025-11-04 15:52:16 -0600

    first commit

---
+++ db/connect.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Database is already initialized!');
+    return callback(null, _db);
+  }
+
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      _db = client.db('contactsDB');
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error('Database not initialized');
+  }
+  return _db;
+};
+
+module.exports = { initDb, getDb };
